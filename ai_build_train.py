175c72c90b2797fab948463c3035df2a426e6024
CatFoolYou <95507639+catfoolyou@users.noreply.github.com>
2023-01-10 10:05:56 -0500

    Add files via upload

--- ai_build_train.py
+++ ai_build_train.py
@@ -7,8 +7,6 @@ keywords = ["doom", "math", "among", "bot", "hello"]
 responses = ["yes", "i hate math", "AMOg uS!?!?", "We have detected automated abuse of Google coming from your Internet Service Provider. This page checks to see if it's really a human sending the requests and not a robot coming through your ISP's network. IP address: 151.188.137.226 Time: 2023-01-03T19:08:42Z", random.choice(greeting)]
 randomwords = ["yes", "no", "idk", "sorry, i dont undersand", "I need to learn more", "sure man", "im not a bot"]
 
-train = False # doesnt work yet
-
 while True:
     userinput = input()
     userinput = userinput.lower()
@@ -25,7 +23,7 @@ while True:
             responses.append(userinput)
 
         
-    if (keyFound == False and train == False):
+    if (keyFound == False):
         print("AI: " + random.choice(randomwords))
         randomwords.append(userinput)
 
